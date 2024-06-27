d7912b001eac2fcb79747f13bb8b399734e62b9d
TeBox <145404809+TeBoxx@users.noreply.github.com>
2024-06-27 15:30:41 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2024-06-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到Cries00空投笔记博客', url: 'https://tebox.top' },
-    { title: '找到你所需要得', url: 'https://tebox.top' }
+    { title: '欢迎来到Cries的空投笔记，博客仅作为记录使用，访问博客前请点击此处阅读本站声明', url: 'https://tebox.top' },
+    { title: 'Welcome to Cries’ Airdrop Notes. This blog is intended for record-keeping purposes only. Please click here to read our disclaimer before visiting the blog.', url: 'https://tebox.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,7 +15,7 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '撸毛笔记',
+  HEO_HERO_TITLE_1: '',
   HEO_HERO_TITLE_2: '',
   HEO_HERO_TITLE_3: '',
   HEO_HERO_TITLE_4: '',
@@ -23,9 +23,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://tebox.top',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门项目', url: '/tag/热门项目' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '每日必看', url: '/tag/每日必看' },
+  HEO_HERO_CATEGORY_2: { title: '每日任务', url: '/tag/每日任务' },
+  HEO_HERO_CATEGORY_3: { title: '空投记录', url: '/tag/空投记录' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
